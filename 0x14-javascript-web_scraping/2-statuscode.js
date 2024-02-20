eac9d7e39afddef3fa6aaa0187416c528bebde17
Edgar Nyamekye Agongo <kwakuedgar@gmail.com>
2024-02-20 14:17:20 -0800

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,10 @@
-i#!/usr/bin/node
+#!/usr/bin/node
+/* displays the status code of a GET request.
+    - the first argument is the URL to request (GET).
+    - output format: 'code: <status code>'.
+*/
 const request = require('request');
-const url = process.argv[2];
-
-request.get(url, (error, response) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(`code: ${response.statusCode}`);
-  }
+request(process.argv[2], function (error, response) {
+  if (error) console.log(error);
+  else console.log('code: ' + response.statusCode);
 });
